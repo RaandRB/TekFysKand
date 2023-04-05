fd1b666a00c248a0d88cfdce8d35c0e66218884e
RaandRB <rasmuspostbox@gmail.com>
2023-04-05 16:25:30 +0200

    fft

--- fykand.py
+++ fykand.py
@@ -4,40 +4,34 @@ import binarystate as bst
 import progress, models, rhs, rk4
 import entanglement as ent
 
-J = [2,1,3]
-
-#spU = np.transpose([1,0])
-#spD = np.transpose([0,1])
 
 #initial conditions
 a = np.zeros(8)
-a[0] = 1/np.sqrt(4)
-a[2] = 1/np.sqrt(4) 
+a[0] = 1/np.sqrt(4) 
 a[7] = 1/np.sqrt(4)
-a[4] = 1/np.sqrt(4)
-
+a[1] = 1/np.sqrt(4)
+a[5] = 1/np.sqrt(4)
 paulix = np.array([(0,1),(1,0)]) 
 pauliy = np.array([(0,-1j),(1j,0)])  
 pauliz = np.array([(1,0),(0,-1)])
 id2 = np.matrix(np.eye(2))
-
-#spin-spin
-A1 = np.array([paulix, paulix, pauliz])
-A2 = np.array([pauliy, paulix, pauliz])
-A3 = np.array([pauliz, pauliz, paulix])
-A4 = np.array([id2, paulix, paulix])
 
 #zeeman
 Z1 = np.array([pauliz, id2, id2])
 Z2 = np.array([id2, pauliz, id2])
 Z3 = np.array([id2, id2, pauliz])
 
-H = np.array([Z1, Z2, Z3, A1, A2, A3, A4])
+H = np.array([Z1, Z2, Z3])
+#A = models.DM([1,0,0])
+A = models.DMCircle([1,0,0])
+A2 = models.threeParticleLine(
+        A2 = models.threeParticleLine())
+H = np.append(H, A, axis=0)
 
-T = 10
-
-h_t = 0.1
-m_t = int(T/h_t+1)
+T_end = 10
+h_t = 0.01
+m_t = int(T_end/h_t+1)
+T = np.linspace(0, T_end, m_t)
 sol = np.zeros([m_t,8], dtype=np.complex128)
 tretangel = np.zeros(m_t)
 tretangel[0] = ent.entanglement(a)
@@ -56,15 +50,39 @@ for tid in range(m_t-1):
 progress.end_progress()
 ABS = np.array([np.absolute(i)**2 for i in sol[:]])
 
+figfourier, axsfourier = plt.subplots(8)
+for i in range(8):
+    fourier = np.absolute(np.fft.fft(ABS[:,i]))
+    freq = np.fft.fftfreq(m_t, h_t)
+    axsfourier[i].set_xlim([-20,20])
+    axsfourier[i].plot(freq, fourier)
+
+#plt.figure('FFT')
+#print(np.shape(ABS[:,0]))
+#fourier = np.fft.fft(ABS[:,0])
+#freq = np.fft.fftfreq(m_t)
+#print(freq)
+#print(fourier)
+#plt.plot(freq, np.absolute(fourier))
+
+
 fig, axs = plt.subplots(8)
 for i in range(8):
     axs[i].set_ylim([0,1])
-    axs[i].plot(range(m_t),ABS[:,i])
+    axs[i].plot(T,ABS[:,i])
 
-#plt.figure('Normalisering')
-#superpos = np.array([np.sum(ABS[i,:]) for i in range(m_t)])
-#plt.plot(range(m_t),superpos)
+plt.figure('Normalisering')
+superpos = np.array([np.sum(ABS[i,:]) for i in range(m_t)])
+plt.plot(range(m_t),superpos)
 
 plt.figure('Tretangel')
 plt.plot(range(m_t),tretangel)
+
+plt.figure('Tretangel fourier')
+four = np.fft.fft(tretangel)
+ffreq = np.fft.fftfreq(m_t, h_t)
+plt.plot(ffreq, np.absolute(four))
+plt.ylim([0,50])
 plt.show()
+
+
--- models.py
+++ models.py
@@ -4,33 +4,156 @@ pauliy = np.array([(0,-1j),(1j,0)])
 pauliz = np.array([(1,0),(0,-1)])
 id2 = np.matrix(np.eye(2))
 
-def heisenbergLine():
+def heisenbergLine(J=[1,1,1]):
     #1 och 2
-    A1 = np.array([paulix, paulix, id2])
-    A2 = np.array([pauliy, pauliy, id2])
-    A3 = np.array([pauliz, pauliz, id2])
+    A1_1 = J[0]*np.array([paulix, paulix, id2])
+    A1_2 = J[0]*np.array([pauliy, pauliy, id2])
+    A1_3 = J[0]*np.array([pauliz, pauliz, id2])
 
     #2 och 3
-    A4 = np.array([id2, paulix, paulix])
-    A5 = np.array([id2, pauliy, pauliy])
-    A6 = np.array([id2, pauliz, pauliz])
+    A2_1 = J[1]*np.array([id2, paulix, paulix])
+    A2_2 = J[1]*np.array([id2, pauliy, pauliy])
+    A2_3 = J[1]*np.array([id2, pauliz, pauliz])
 
-    return A1,A2,A3,A4,A5,A6
+    return np.array([A1_1,A1_2,A1_3,A2_1,A2_2,A2_3])
 
-def heisenbergCircle():
+def heisenbergCircle(J=[1,1,1]):
     #1 och 2
-    A1 = np.array([paulix, paulix, id2])
-    A2 = np.array([pauliy, pauliy, id2])
-    A3 = np.array([pauliz, pauliz, id2])
+    A1_1 = J[0]*np.array([paulix, paulix, id2])
+    A1_2 = J[0]*np.array([pauliy, pauliy, id2])
+    A1_3 = J[0]*np.array([pauliz, pauliz, id2])
 
     #2 och 3
-    A4 = np.array([id2, paulix, paulix])
-    A5 = np.array([id2, pauliy, pauliy])
-    A6 = np.array([id2, pauliz, pauliz])
+    A2_1 = J[1]*np.array([id2, paulix, paulix])
+    A2_2 = J[1]*np.array([id2, pauliy, pauliy])
+    A2_3 = J[1]*np.array([id2, pauliz, pauliz])
 
     #1 och 3
-    A7 = np.array([paulix, id2, paulix])
-    A8 = np.array([pauliy, id2, pauliy])
-    A9 = np.array([pauliz, id2, pauliz])
+    A3_1 = J[2]*np.array([paulix, id2, paulix])
+    A3_2 = J[2]*np.array([pauliy, id2, pauliy])
+    A3_3 = J[2]*np.array([pauliz, id2, pauliz])
 
-    return A1,A2,A3,A4,A5,A6,A7,A8,A9
+    return np.array([A1_1,A1_2,A1_3,A2_1,A2_2,A2_3,A3_1,A3_2,A3_3])
+
+def DMLine(d=[0,0,1]):
+    #s1 x s2
+    #x
+    A1_1 = d[0]*np.array([pauliy,pauliz,id2])
+    A1_2 = d[0]*-1*np.array([pauliz,pauliy,id2])
+    #y
+    A1_3 = d[1]*-1*np.array([paulix,pauliz,id2])
+    A1_4 = d[1]*np.array([pauliz,paulix,id2])
+    #z
+    A1_5 = d[2]*np.array([paulix,pauliy,id2])
+    A1_6 = d[2]*-1*np.array([pauliy,paulix,id2])
+
+    #s1 x s3
+    #x
+    A2_1 = d[0]*np.array([pauliy,id2,pauliz])
+    A2_2 = d[0]*-1*np.array([pauliz,id2,pauliy])
+    #y
+    A2_3 = d[1]*-1*np.array([paulix,id2,pauliz])
+    A2_4 = d[1]*np.array([pauliz,id2,paulix])
+    #z
+    A2_5 = d[2]*np.array([paulix,id2,pauliy])
+    A2_6 = d[2]*-1*np.array([pauliy,id2,paulix])
+    
+    return np.array([A1_1,A1_2,A1_3,A1_4,A1_5,A1_6,A2_1,A2_2,A2_3,A2_4,A2_5,A2_6])
+
+def DMCircle(d=[0,0,1]):
+    #s1 x s2
+    #x
+    A1_1 = d[0]*np.array([pauliy,pauliz,id2])
+    A1_2 = d[0]*-1*np.array([pauliz,pauliy,id2])
+    #y
+    A1_3 = d[1]*-1*np.array([paulix,pauliz,id2])
+    A1_4 = d[1]*np.array([pauliz,paulix,id2])
+    #z
+    A1_5 = d[2]*np.array([paulix,pauliy,id2])
+    A1_6 = d[2]*-1*np.array([pauliy,paulix,id2])
+
+    #s1 x s3
+    #x
+    A2_1 = d[0]*np.array([pauliy,id2,pauliz])
+    A2_2 = d[0]*-1*np.array([pauliz,id2,pauliy])
+    #y
+    A2_3 = d[1]*-1*np.array([paulix,id2,pauliz])
+    A2_4 = d[1]*np.array([pauliz,id2,paulix])
+    #z
+    A2_5 = d[2]*np.array([paulix,id2,pauliy])
+    A2_6 = d[2]*-1*np.array([pauliy,id2,paulix])
+
+    #s2 x s3
+    #x
+    A3_1 = d[0]*np.array([id2,pauliy,pauliz])
+    A3_2 = d[0]*-1*np.array([id2,pauliz,pauliy])
+    #y
+    A3_3 = d[1]*-1*np.array([id2,paulix,pauliz])
+    A3_4 = d[1]*np.array([id2,pauliz,paulix])
+    #z
+    A3_5 = d[2]*np.array([id2,paulix,pauliy])
+    A3_6 = d[2]*-1*np.array([id2,pauliy,paulix])
+    
+    return np.array([A1_1,A1_2,A1_3,A1_4,A1_5,A1_6,A2_1,A2_2,A2_3,A2_4,A2_5,A2_6,A3_1,A3_2,A3_3,A3_4,A3_5,A3_6])
+
+def threeParticleLine(d=[1,1,1]):
+    #s3*(s1 x s2)
+    #x
+    A1_1 = d[0]*np.array([pauliy,pauliz,paulix])
+    A1_2 = d[0]*-1*np.array([pauliz,pauliy,paulix])
+    #y
+    A1_3 = d[0]*-1*np.array([paulix,pauliz,pauliy])
+    A1_4 = d[0]*np.array([pauliz,paulix,pauliy])
+    #z
+    A1_5 = d[0]*np.array([paulix,pauliy,pauliz])
+    A1_6 = d[0]*-1*np.array([pauliy,paulix,pauliz])
+
+    #s2*(s1 x s3)
+    #x
+    A2_1 = d[0]*np.array([pauliy,paulix,pauliz])
+    A2_2 = d[0]*-1*np.array([pauliz,paulix,pauliy])
+    #y
+    A2_3 = d[1]*-1*np.array([paulix,pauliy,pauliz])
+    A2_4 = d[1]*np.array([pauliz,pauliy,paulix])
+    #z
+    A2_5 = d[2]*np.array([paulix,pauliz,pauliy])
+    A2_6 = d[2]*-1*np.array([pauliy,pauliz,paulix])
+
+    return np.array([A1_1,A1_2,A1_3,A1_4,A1_5,A1_6,A2_1,A2_2,A2_3,A2_4,A2_5,A2_6])
+
+
+def threeParticleCircle(d=[1,1,1]):
+    #s3*(s1 x s2)
+    #x
+    A1_1 = d[0]*np.array([pauliy,pauliz,paulix])
+    A1_2 = d[0]*-1*np.array([pauliz,pauliy,paulix])
+    #y
+    A1_3 = d[0]*-1*np.array([paulix,pauliz,pauliy])
+    A1_4 = d[0]*np.array([pauliz,paulix,pauliy])
+    #z
+    A1_5 = d[0]*np.array([paulix,pauliy,pauliz])
+    A1_6 = d[0]*-1*np.array([pauliy,paulix,pauliz])
+
+    #s2*(s1 x s3)
+    #x
+    A2_1 = d[0]*np.array([pauliy,paulix,pauliz])
+    A2_2 = d[0]*-1*np.array([pauliz,paulix,pauliy])
+    #y
+    A2_3 = d[1]*-1*np.array([paulix,pauliy,pauliz])
+    A2_4 = d[1]*np.array([pauliz,pauliy,paulix])
+    #z
+    A2_5 = d[2]*np.array([paulix,pauliz,pauliy])
+    A2_6 = d[2]*-1*np.array([pauliy,pauliz,paulix])
+
+    #s1*(s2 x s3)
+    #x
+    A3_1 = d[0]*np.array([paulix,pauliy,pauliz])
+    A3_2 = d[0]*-1*np.array([paulix,pauliz,pauliy])
+    #y
+    A3_3 = d[1]*-1*np.array([pauliy,paulix,pauliz])
+    A3_4 = d[1]*np.array([pauliy,pauliz,paulix])
+    #z
+    A3_5 = d[2]*np.array([pauliz,paulix,pauliy])
+    A3_6 = d[2]*-1*np.array([pauliz,pauliy,paulix])
+
+    return np.array([A1_1,A1_2,A1_3,A1_4,A1_5,A1_6,A2_1,A2_2,A2_3,A2_4,A2_5,A2_6,A3_1,A3_2,A3_3,A3_4,A3_5,A3_6])
