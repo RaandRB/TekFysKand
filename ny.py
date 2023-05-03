7a53233b7639f90046a8548d08220d0972bee8bc
RaandRB <rasmuspostbox@gmail.com>
2023-05-03 10:39:22 +0200

    prediction shurda

--- ny.py
+++ ny.py
@@ -5,58 +5,85 @@ import progress, models2, rk4
 import rhs2, rhs
 import entanglement as ent
 import shurda as sh
-import fractions
+import fractions as fr
+import math
+from decimal import Decimal
 
 #initial conditions
 a = np.zeros(8, dtype=np.complex128)
-a[0] = 1/np.sqrt(2)
-a[7] = 1/np.sqrt(2)
-
+om = np.exp(2j/3*np.pi)
+a[0:8] = 1
 
 a = a/np.linalg.norm(a)
 
 a0 = a
-
-#psi = sh.initial(a)
-#print(psi)
 
 paulix = np.array([[0,1],[1,0]])
 pauliy = np.array([[0,-1j],[1j,0]])
 pauliz = np.array([[1,0],[0,-1]])
 id2 = np.matrix(np.eye(2))
 
-#zeeman
-Z1 = np.array([pauliz, id2, id2])
-Z2 = np.array([id2, pauliz, id2])
-Z3 = np.array([id2, id2, pauliz])
-ZZ = np.array([Z1,Z2,Z3])
-XX = np.array([np.array([paulix, paulix, paulix])])
-
 Zeeman = sh.Ham(pauliz,id2,id2) + sh.Ham(id2,pauliz,id2) + sh.Ham(id2,id2,pauliz)
 
+H = models2.DMLine() + 0.01*Zeeman + 0.01*models2.heisenbergLine()
 
-H = models2.DMCircle([1,1,0])
-w, v = np.linalg.eig(H)
+w, v = np.linalg.eig((H))
 w = np.real(w)
-
-#print(w)
-#ratio1 = w[0]/w[7]
-#ratio2 = w[0]/w[3]
-#test1 = fractions.Fraction(ratio1)
-#test2 = fractions.Fraction(ratio2)
-#print((test1, test2))
-
-#numerator1 = test1.numerator
-#numerator2 = test2.numerator
-#prime1 = sh.prime_factors(np.abs(numerator1))
-#prime2 = sh.prime_factors(np.abs(numerator2))
-
-#print(prime1)
-#print(prime2)
+#for i in v:
+#    print(np.transpose(a)@np.resize(i,8))
+print(w)
 
 
-#A = models.DM([1,0,0])
-T_end = 10
+#fractions = [fr.Fraction(Decimal(str(i))) for i in w2]
+
+#m = [i.denominator for i in fractions]
+#n = [i.numerator for i in fractions]
+
+
+#print(fractions)
+#lcm = 1
+#for i in n:
+#    lcm = lcm*i//np.gcd(lcm, i)
+#gcd = math.gcd(*m)
+#print((lcm,gcd))
+#revivalTime = 2*np.pi*lcm/gcd
+#print(revivalTime)
+w3 = np.abs(w)
+w3 = np.sort(w3)
+
+print(w3)
+
+count1 = sum(i<0 for i in w3[0:4])
+count2 = sum(i<0 for i in w3[4:8])
+
+
+lowerw = np.array(w3[0:4])
+upperw = np.array(w3[4:8])
+plw = np.polyfit(np.array(range(4)), lowerw, 3)
+puw = np.polyfit(np.array(range(4)), upperw, 3)
+
+
+test_t1 = 2*np.pi/lowerw[0]
+test_t2 = 2*np.pi/upperw[0]
+
+kep_t1 = 2*np.pi/lowerw[1]
+kep_t2 = 2*np.pi/upperw[1]
+
+res_t1 = 2*np.pi/(1/2*lowerw[2])
+res_t2 = 2*np.pi/(1/2*upperw[2])
+
+supres_t1 = 2*np.pi/(1/6*lowerw[3])
+supres_t2 = 2*np.pi/(1/6*upperw[3])
+
+print(test_t1, test_t2)
+
+print(kep_t1, kep_t2)
+
+print(res_t1, res_t2)
+
+print(supres_t1, supres_t2)
+
+T_end = 2000
 h_t = 0.01
 m_t = int(T_end/h_t+1)
 T = np.linspace(0, T_end, m_t)
@@ -67,7 +94,7 @@ prob = np.zeros(m_t)
 prob[0] = 1
 sol[0] = np.flip(a)
 t = 0
-progress.start_progress("Calculating...")
+progress.start_progress("Calculating")
 
 for tid in range(m_t-1):
     x = tid/m_t*100
@@ -75,7 +102,7 @@ for tid in range(m_t-1):
     a, t = rk4.step(rhs2.rhs, a, t, h_t, H)
     sol[tid+1,:] = np.flip(a)
     tretangel[tid+1] = ent.entanglement(np.flip(a))
-    prob[tid+1] = np.abs((np.transpose(a0)@a))**2
+    prob[tid+1] = np.abs((np.conjugate(a0)@a))**2
     #if prob[tid+1] >= 0.98:
     #    print(tid)
 
@@ -83,26 +110,44 @@ progress.end_progress()
 
 ABS = np.array([np.absolute(i) for i in sol[:]])
 
-fig, axs = plt.subplots(8)
-for i in range(8):
-    axs[i].set_ylim([0,1])
-    axs[i].plot(range(m_t),ABS[:,i])
+#fig, axs = plt.subplots(8)
+#for i in range(8):
+#    axs[i].set_ylim([0,1])
+#    axs[i].plot(range(m_t),ABS[:,i])
 
-fig2, axs2 = plt.subplots(8)
-for i in range(8):
-    axs2[i].set_ylim([-1,1])
-    axs2[i].plot(range(m_t), np.real(sol[:,i]), 'r')
-    axs2[i].plot(range(m_t), np.imag(sol[:,i]), 'b')
+#fig2, axs2 = plt.subplots(8)
+#for i in range(8):
+#    axs2[i].set_ylim([-1,1])
+#    axs2[i].plot(range(m_t), np.real(sol[:,i]), 'r')
+#    axs2[i].plot(range(m_t), np.imag(sol[:,i]), 'b')
+
+
+#sq = np.array([np.sum(ABS[i,:]**2) for i in range(m_t)])
+#plt.figure('Normalisering')
+#ax = plt.gca()
+#ax.set_ylim([0,1.1])
+#plt.plot(range(m_t), sq)
 
 diff = np.absolute(prob-tretangel)
 
-plt.figure('Tretangel och Prob')
-plt.plot(range(m_t), tretangel,'r', label='Residual entanglement')
-plt.plot(range(m_t), prob, 'b', label='Survival probability')
-plt.legend(loc='upper left')
+plt.figure('Residual entanglement')
+plt.plot(T, tretangel,'r', label='Residual entanglement')
+plt.figure('Survival probability')
+plt.plot(T, prob, 'b', label='Survival probability')
+plt.axvline(x=test_t1, color='y')
+plt.axvline(x=test_t2, color='r')
+plt.axvline(x=kep_t1, color ='y')
+plt.axvline(x=kep_t2, color='r')
+plt.axvline(x=res_t1, color='y')
+plt.axvline(x=res_t2, color='r')
+plt.axvline(x=supres_t1, color='y')
+plt.axvline(x=supres_t2, color='r')
+#plt.legend(loc='upper left')
 
-plt.figure('Diff')
-plt.plot(range(m_t), diff)
+
+
+#plt.figure('Diff')
+#plt.plot(range(m_t), diff)
 
 plt.show()
 
